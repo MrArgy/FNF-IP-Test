6ca8b58f69045f1c0a6a7db98309c11451d3fd5b
MrArgy <95936243+MrArgy@users.noreply.github.com>
2022-06-15 23:48:32 +0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -458,7 +458,7 @@ class TitleState extends MusicBeatState
 					if (mustUpdate) {
 						MusicBeatState.switchState(new OutdatedState());
 					} else {
-						MusicBeatState.switchState(new MainMenuState());
+						MusicBeatState.switchState(new OutdatedSubState());
 					}
 					closedState = true;
 				});
